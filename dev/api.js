4016f176b63d9c750642e913eec9290ecf0f85b1
Umorjyoti Chetia <umorjyotichetia@gmail.com>
2022-06-19 20:15:43 +0530

    transaction endpoint added

--- dev/api.js
+++ dev/api.js
@@ -13,6 +13,11 @@ app.get('/blockchain', function (req, res) {
   res.send(bitcoin);
 });
 
+app.post('/transaction', function (req, res) {
+    const blockIndex = bitcoin.createtransaction(req.body.amount,req.body.sender,req.body.recipient);
+    res.json({"note":`Transaction will be added in block ${blockIndex}`});
+  });
+
 app.listen(3000,()=>{
     console.log("Server started listening on port 3000...")
 });
